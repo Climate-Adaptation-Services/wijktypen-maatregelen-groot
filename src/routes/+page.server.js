a5158f39bd48d42f79d7a3b86cc58c0a480d3755
stichtingcas <daniel@climateadaptationservices.com>
2023-08-14 12:11:58 +0200

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { dsv } from 'd3'
 
 export function load(){
-  const data = dsv(';', "https://gist.githubusercontent.com/stichtingcas/3d474b72da6142a18244677ac8a38b9d/raw/9173c2919f5fab630951d70e6304b9b0e59db24d/gemiddelde-ingrepen.csv")
+  const data = dsv(';', "https://gist.githubusercontent.com/stichtingcas/38c68f15acfb359c1a1e7dd48167ff0f/raw/2154f59a57bdb3ed887f628ffade8101ed48980c/grote-ingrepen.csv")
   return { data }
 }
 
